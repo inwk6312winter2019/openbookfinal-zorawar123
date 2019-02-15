1580883aacc5afac0fe20df2dbda496653d47303
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-15 11:29:09 -0800

    Test

--- Task02_A.py
+++ Task02_A.py
@@ -1,10 +1,30 @@
-file  = open("Book1.py")
-def  count_the_article(file):
-  list1 => ["a", "the", "at", "run", "to","and","are","or","for","an","this"]
-  for word in file:
-    word.strip()
-    word.strip() # convert the string into list
-    for words in word:
-      if list1 in words:
-        print(Counter(list1)) # print the dictinary with the key as article and  it cou$
-count_the_article(file)
+import requests
+
+# Import 
+import json
+
+controller='sandboxdnac.cisco.com'
+
+def getTicket():
+	
+	url = "https://" + controller + "/api/v1/ticket"
+
+	# The username and password to access the DNA Center
+	payload = {"username":"devnetuser","password":"Cisco123!"}
+
+	# Content type must be included in the header
+	header = {"content-type": "application/json"}
+
+	# Perform a POST on the specified url to get the token
+	response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
+
+	# Convert response to JSON format
+	res_json=response.json()
+
+	# Parse the JSON to get the token
+	ticket = res_json["response"]["serviceTicket"]
+
+	return ticket
+
+getTicket()
+
